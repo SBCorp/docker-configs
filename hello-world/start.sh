2560badd55f175a4ebd335d668ddb0e056f9a7e9
Determe, Sebastien (sd378r) <sd378r@intl.att.com>
2016-10-12 03:34:03 -0700

    Fix pull and run command

--- hello-world/start.sh
+++ hello-world/start.sh
@@ -14,8 +14,8 @@ test_params
 echo "Logging docker engine $DOCKER_ADDRESS in to $REGISTRY_ADDRESS"
 echo $DOCKER_PASSWORD| sudo -S docker login -u=$REGISTRY_LOGIN -p=$REGISTRY_PASSWORD https://$REGISTRY_ADDRESS
 
-echo "Pulling the image named $REGISTRY_ADDRESS/$IMAGE_NAME from docker engine $DOCKER_ADDRESS "
-docker -H $DOCKER_ADDRESS pull $REGISTRY_ADDRESS/$IMAGE_NAME
+echo "Pulling the image named $REGISTRY_ADDRESS/$IMAGE_NAME:$IMAGE_VERSION from docker engine $DOCKER_ADDRESS "
+docker -H $DOCKER_ADDRESS pull $REGISTRY_ADDRESS/$IMAGE_NAME:$IMAGE_VERSION
 
 echo "Starting image $REGISTRY_ADDRESS/$IMAGE_NAME on Docker engine $DOCKER_ADDRESS"
-docker -H $DOCKER_ADDRESS run -rm --name hello-world-demo --hostname hello-world-demo -p 18080:8080 -v `pwd`/conf/image/:hello-world/conf $REGISTRY_ADDRESS/$IMAGE_NAME
+docker -H $DOCKER_ADDRESS run -rm --name hello-world-demo --hostname hello-world-demo -p 18080:8080 -v `pwd`/conf/image/:hello-world/conf $REGISTRY_ADDRESS/$IMAGE_NAME:$IMAGE_VERSION
